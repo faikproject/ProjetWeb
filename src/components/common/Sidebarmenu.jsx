ec89302aab9824a46d461c86a52a5501015a99d6
YuSONG <songyufivo@gmail.com>
2023-06-17 12:32:38 +0200

    creation les pages

---
+++ src/components/common/Sidebarmenu.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+export const Sidebarmenu = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'About me',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Gallery',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'News',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Facebook',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Instagram',
+    path: '/',
+    cName: 'nav-text'
+  }
+];
+
